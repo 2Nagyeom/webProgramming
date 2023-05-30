c84d0c04ab13b61a9ed121be07b06b140b260e3b
2Nagyeom <70017526+2Nagyeom@users.noreply.github.com>
2023-05-31 00:59:55 +0900

    특정 위치까지 도착하면 멈추게 하기

--- component/bottomTab/index.js
+++ component/bottomTab/index.js
@@ -1,11 +1,54 @@
 export default function bottomTab() {
-    let template = `
-      <div id="bottomSheet" class="navbar">
-        <a href="#home">홈메뉴</a>
-        <a href="#chatbot">chat</a>
-        <a href="#mypage">마이페이지</a>
+  let template = `
+    <div id="bottomSheet" class="navbar">
+    <div class="navlist">
+        <div class="navcontent">
+          <p class="contents">치안센터</p>
+        </div>
+        <div class="navcontent">
+          <p class="contents">범죄발생현황</p>
+        </div>
+        <div class="navcontent">
+          <p class="contents">복지시설</p>
+        </div>
+        <div class="navcontent">
+          <p class="contents">의료시설</p>
+        </div>
+        <div class="navcontent">
+          <p class="contents">선호시설</p>
+        </div>
       </div>
-    `;
-    return template;
-  }
+    <div class="contentslist">ㅎㅇ</div>
+  </div>
+  `;
+  return template;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  const bottomSheet = document.getElementById('bottomSheet');
+  let prevClickedDiv = null;
+
+  bottomSheet.addEventListener('click', function(event) {
+    if (event.target.classList.contains('navcontent')) {
+      event.target.style.backgroundColor = 'red';
+      event.target.style.color = 'white';
+    }
+  });
+  bottomSheet.addEventListener('click', function(event) {
+    const clickedDiv = event.target.closest('.navcontent');
   
+    if (clickedDiv) {
+      if (prevClickedDiv) {
+        // 이전에 클릭한 div를 원래 상태로 되돌립니다.
+        prevClickedDiv.style.backgroundColor = ''; // 기본 배경색으로 변경
+        prevClickedDiv.style.color = ''; // 기본 텍스트 색상으로 변경
+      }
+  
+      clickedDiv.style.backgroundColor = 'red'; // 클릭한 div의 스타일을 변경합니다.
+      clickedDiv.style.color = 'white';
+  
+      // 현재 클릭한 div를 prevClickedDiv 변수에 저장합니다.
+      prevClickedDiv = clickedDiv;
+    }
+  });
+});
--- utils.js
+++ utils.js
@@ -1,35 +1,48 @@
 export function bottomSheetUtils() {
-    
     let isDragging = false;
-    const bottomSheet = document.getElementById('bottomSheet')
-   let dragElement = document.getElementById('bottomSheet');
-   let initialY = 0;
-   let currentY = 0;
-   
-   dragElement.addEventListener('mousedown', startDrag);
-   dragElement.addEventListener('mousemove', drag);
-   dragElement.addEventListener('mouseup', stopDrag);
-   
-   function startDrag(event) {
-     isDragging = true;
-     if(initialY === 0){
-         initialY = event.clientY;
-     }
-     console.log(event.clientY);
-   }
-   
-   function drag(event) {
-     if (isDragging) {
-       currentY = event.clientY - initialY;
-       dragElement.style.transform = `translateY(${currentY}px)`;
-     }
-   }
-   
-   function stopDrag() {
-     isDragging = false;
-     initialY = currentY
-     console.log(initialY,'stop drag initial Y');
-     //dragElement.style.transform = 'none';
-   }
-
-}
+    const bottomSheet = document.getElementById('bottomSheet');
+    let dragElement = document.getElementById('bottomSheet');
+    let initialY = 0;
+    let currentY = 0;
+    let targetY = 523; // 목표 위치 (예: 200px)
+    let isReturning = false; // 추가된 변수: 원래 위치로 돌아가는 중인지 여부
+  
+    dragElement.addEventListener('mousedown', startDrag);
+    dragElement.addEventListener('mousemove', drag);
+    dragElement.addEventListener('mouseup', stopDrag);
+  
+    function startDrag(event) {
+      isDragging = true;
+      if (!isReturning) { // 원래 위치로 돌아가는 중이 아닐 때만 초기화
+        initialY = event.clientY;
+        currentY = event.clientY - initialY;
+      }
+      console.log(event.clientY);
+    }
+  
+    function drag(event) {
+      if (isDragging) {
+        if (!isReturning) { // 원래 위치로 돌아가는 중이 아닐 때만 드래그 적용
+          currentY = event.clientY - initialY;
+          if (currentY < targetY) {
+            dragElement.style.transform = `translateY(${currentY}px)`;
+          } else {
+            dragElement.style.transform = `translateY(${targetY}px)`;
+            stopDrag();
+          }
+        }
+      }
+    }
+  
+    function stopDrag() {
+      isDragging = false;
+      if (currentY >= targetY) {
+        isReturning = true;
+        dragElement.style.transform = 'translateY(0)';
+      } else {
+        initialY = currentY;
+      }
+      console.log(initialY, 'stop drag initial Y');
+    }
+  }
+  
